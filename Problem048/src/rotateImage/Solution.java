61046252c5c174b6959deb8331c7942b0be54bf7
Wenzhao Liu <zmaxlwz@gmail.com>
2016-08-20 09:31:42 -0400

    add problem 48
    add problem 48

--- Problem048/src/rotateImage/Solution.java
+++ Problem048/src/rotateImage/Solution.java
@@ -5,18 +5,18 @@ public class Solution {
 	public void rotate(int[][] matrix){
 		//rotate the n*n image by 90 degrees clockwise
 		//do it in-place
-		
+
 		if(matrix == null || matrix.length==0){
 			return;
 		}
-		
+
 		int rowNum = matrix.length;
 		int colNum = matrix[0].length;
-		
+
 		//row number is not equal to column number
 		if(rowNum != colNum)
 			return;
-		
+
 		//first swap according to up diagnal
 		for(int i=0;i<rowNum-1;i++){
 			for(int j=0;j<colNum-1-i;j++){
@@ -26,7 +26,7 @@ public class Solution {
 				matrix[rowNum-1-j][colNum-1-i] = temp;
 			}
 		}
-				
+
 		//then swap according to x middle line
 		for(int i=0;i<rowNum/2;i++){
 			for(int j=0;j<colNum;j++){
@@ -35,9 +35,32 @@ public class Solution {
 				matrix[rowNum-1-i][j] = temp;
 			}
 		}
-				
+
 	}
-	
+
+	public void rotate2(int[][] matrix){
+		if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+			return;
+		}
+		int numRows = matrix.length;
+		int numCols = matrix[0].length;
+		for(int i =0;i<numRows-1;i++){
+			for(int j=i+1;j<numCols;j++){
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+		for(int i =0;i<numRows;i++){
+			for(int j=0;j<numCols/2;j++){
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[i][numCols-1-j];
+				matrix[i][numCols-1-j] = temp;
+			}
+		}
+		return;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -49,14 +72,14 @@ public class Solution {
 			System.out.println();
 		}	
 		Solution sol = new Solution();
-		sol.rotate(matrix);
+		sol.rotate2(matrix);
 		for(int i=0;i<matrix.length;i++){
 			for(int j=0;j<matrix[0].length;j++){
 				System.out.print(matrix[i][j] + ", ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 }
