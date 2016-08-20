2c2b919081ba1a655de6b492b3ef4a91e0845a02
Wenzhao Liu <zmaxlwz@gmail.com>
2016-08-20 09:11:13 -0400

    add problem 32
    add problem 32

--- Problem032/src/longestValidParentheses/Solution.java
+++ Problem032/src/longestValidParentheses/Solution.java
@@ -6,14 +6,14 @@ public class Solution {
 
 	public int longestValidParentheses(String s){
 		//use a stack
-		
+
 		if(s == null || s.length() == 0){
 			return 0;
 		}
-		
+
 		//store the index of parenthese
 		Stack<Integer> stack = new Stack<Integer>();
-		
+
 		char c;
 		int maxLen = 0;
 		for(int i=0; i<s.length();i++){
@@ -42,27 +42,78 @@ public class Solution {
 				}
 			}
 		}
-		
+
 		if(stack.empty()){
 			return s.length();
 		}
 		else{
-			
+
 			maxLen = Math.max(maxLen, s.length()-1-stack.peek());
-			
+
 		}
-		
+
 		return maxLen;
 	}
-	
+
+	public int longestValidParentheses2(String s){
+		if(s==null || s.length() <= 1){
+			return 0;
+		}
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		int length = s.length();
+		int maxLength = 0;
+		for(int i =0;i<length;i++){
+			char c = s.charAt(i);
+
+			if(c == '('){
+				if(stack.isEmpty()){
+					maxLength = Math.max(maxLength, i);
+				}
+				else{
+					int index = stack.peek();
+					maxLength = Math.max(maxLength, i-1-index);
+				}
+				stack.push(i);
+			}
+			else{
+				if(stack.isEmpty()){
+					maxLength = Math.max(maxLength, i);
+					stack.push(i);
+				}
+				else{
+					int index = stack.peek();
+					if(s.charAt(index) == '('){
+						stack.pop();
+						continue;
+					}
+					else{
+						maxLength = Math.max(maxLength, i-1-index);
+						stack.push(i);
+					}
+				}
+
+			}
+		}
+		
+		if(stack.isEmpty()){
+			return length;
+		}
+		int index = stack.peek();
+		maxLength = Math.max(maxLength, length-1-index);
+		
+		return maxLength;
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//String s = "()";
 		//String s = "())";
+		//String s = ")()())";
 		String s = ")(((((()())()()))()(()))(";
 		Solution sol = new Solution();
-		System.out.println(sol.longestValidParentheses(s));
+		System.out.println(sol.longestValidParentheses2(s));
 	}
 
 }
