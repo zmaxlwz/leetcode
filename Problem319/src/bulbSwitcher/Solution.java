310f83e958014c047f8972c5cbb957a0f44cad39
Wenzhao Liu <zmaxlwz@gmail.com>
2016-11-23 11:24:31 -0500

    add problem 319
    add problem 319

---
+++ Problem319/src/bulbSwitcher/Solution.java
@@ -0,0 +1,15 @@
+package bulbSwitcher;
+
+public class Solution {
+	
+	public int bulbSwitch(int n) {
+		int num = (int)Math.sqrt(n);
+		return num;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
